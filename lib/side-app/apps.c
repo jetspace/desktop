4a86ab51b83444bdcc7b4f79cd1acd5b17f2e093
Marius Messerschmidt <marius.messerschmidt@googlemail.com>
2015-02-13 21:49:58 +0100

    Version 0.28-2
    Fixed a little bug with the lib
    
    Signed-off-by: Marius Messerschmidt <marius.messerschmidt@googlemail.com>

--- lib/side-app/apps.c
+++ lib/side-app/apps.c
@@ -41,20 +41,21 @@ AppEntry *side_apps_get_next_entry(void)
 		return NULL;
 
 	char buffer[2000];
-
 	while(fgets(buffer, 2000, side_apps_entry_file) != NULL)
 	{
 		if(strncmp(buffer, "Name=", 5) == 0)
 		{//we found the name(!!!)
 			ret->app_name = malloc(strlen(buffer) - 5);
 			strtok(buffer, "=");
-			strcpy(ret->app_name, strtok(NULL, "\n"));
+			char *ptr = strtok(NULL, "\n");
+			strcpy(ret->app_name, ptr != NULL ? ptr : "");
 		}
 		if(strncmp(buffer, "Exec=", 5) == 0)
 		{//EXEC PATH
 			ret->exec = malloc(strlen(buffer) -5);
 			strtok(buffer, "=");
-			strcpy(ret->exec, strtok(NULL, "\n"));
+			char *ptr = strtok(NULL, "\n");
+			strcpy(ret->exec, ptr != NULL ? ptr : "");
 		}
 		if(strncmp(buffer , "Type=", 5) == 0)
 		{//type
@@ -63,66 +64,78 @@ AppEntry *side_apps_get_next_entry(void)
 		}
 		if(strncmp(buffer, "Categories=", 11) == 0)
 		{
-			char *b = malloc(sizeof(buffer) -11);
-			strtok(buffer, "=");
-			strcpy(b, strtok(NULL, "\n"));
 
-
-			//Now phrase a category
-
-			char *ptr = strtok(b, ";");
-
-			ret->sub = APP_TYPE_UNKNOWN;
-
-			while(ptr != NULL)
+			if(strlen(buffer) == 11)
+				ret->sub = APP_TYPE_UNKNOWN; //does not contain categories...
+			else
 			{
-				if(strcmp(ptr, "AudioVideo") == 0 || strcmp(ptr, "Audio") == 0 || strcmp(ptr, "Video") == 0)
-				{
-					ret->sub = APP_TYPE_MULTIMEDIA;
-				}
-				if(strcmp(ptr, "Development") == 0)
-				{
-					ret->sub = APP_TYPE_DEVELOPMENT;
-				}
-				if(strcmp(ptr, "Education") == 0)
-				{
-					ret->sub = APP_TYPE_EDUCATION;
-				}
-				if(strcmp(ptr, "Game") == 0)
-				{
-					ret->sub = APP_TYPE_GAME;
-				}
-				if(strcmp(ptr, "Graphics") == 0)
-				{
-					ret->sub = APP_TYPE_GRAPHICS;
-				}
-				if(strcmp(ptr, "Network") == 0)
-				{
-					ret->sub = APP_TYPE_NETWORK;
-				}
-				if(strcmp(ptr, "Office") == 0)
-				{
-					ret->sub = APP_TYPE_OFFICE;
-				}
-				if(strcmp(ptr, "Science") == 0)
-				{
-					ret->sub = APP_TYPE_SCIENCE;
-				}
-				if(strcmp(ptr, "Settings") == 0)
-				{
-					ret->sub = APP_TYPE_SETTINGS;
-				}
-				if(strcmp(ptr, "System") == 0)
-				{
-					ret->sub = APP_TYPE_SYSTEM;
-				}
-				if(strcmp(ptr, "Utility") == 0)
-				{
-					ret->sub = APP_TYPE_UTILITY;
-				}
+				char *b = malloc(sizeof(buffer) -11);
+				strtok(buffer, "=");
+				strcpy(b, strtok(NULL, "\n"));
 
 
-				ptr = strtok(NULL, ";");
+				//Now phrase a category
+
+				char *ptr = strtok(b, ";\n");
+
+				ret->sub = APP_TYPE_UNKNOWN;
+
+				while(ptr != NULL)
+				{
+					if(strcmp(ptr, "AudioVideo") == 0 || strcmp(ptr, "Audio") == 0 || strcmp(ptr, "Video") == 0)
+					{
+						ret->sub = APP_TYPE_MULTIMEDIA;
+					}
+					if(strcmp(ptr, "Development") == 0)
+					{
+						ret->sub = APP_TYPE_DEVELOPMENT;
+					}
+					if(strcmp(ptr, "Education") == 0)
+					{
+						ret->sub = APP_TYPE_EDUCATION;
+					}
+					if(strcmp(ptr, "Game") == 0)
+					{
+						ret->sub = APP_TYPE_GAME;
+					}
+					if(strcmp(ptr, "Graphics") == 0)
+					{
+						ret->sub = APP_TYPE_GRAPHICS;
+					}
+					if(strcmp(ptr, "Network") == 0)
+					{
+						ret->sub = APP_TYPE_NETWORK;
+					}
+					if(strcmp(ptr, "Office") == 0)
+					{
+						ret->sub = APP_TYPE_OFFICE;
+					}
+					if(strcmp(ptr, "Science") == 0)
+					{
+						ret->sub = APP_TYPE_SCIENCE;
+					}
+					if(strcmp(ptr, "Settings") == 0)
+					{
+						ret->sub = APP_TYPE_SETTINGS;
+					}
+					if(strcmp(ptr, "System") == 0)
+					{
+						ret->sub = APP_TYPE_SYSTEM;
+					}
+					if(strcmp(ptr, "Utility") == 0)
+					{
+						ret->sub = APP_TYPE_UTILITY;
+					}
+
+					for(int x = 0; x < strlen(ptr); x++)
+						{
+							if(ptr[x] != ';')
+								continue;
+							else
+								puts("FOUND");
+						}
+					ptr = strtok(NULL, ";\n");
+				}
 			}
 		}
 
@@ -142,4 +155,3 @@ int side_apps_close(void)
 		return 0;
 
 }
-
