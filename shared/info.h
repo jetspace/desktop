81ae163d86f32963270b5915b6e56ac5236aac2c
Marius Messerschmidt <marius.messerschmidt@googlemail.com>
2015-06-06 14:47:44 +0200

    Version 0.44
    -> Now we have an almost stable notify client...
    -> Volume Window will now hide if it looses focus
    
    P.S.
    I know that it was a long time, since the last updte (>3 months), sorry for that!
    But I did not have much time, so....
    Anyways, I hope I can now work a little bit on this again, might to get to
    .50
    
    Signed-off-by: Marius Messerschmidt <marius.messerschmidt@googlemail.com>

--- shared/info.h
+++ shared/info.h
@@ -7,7 +7,7 @@ For more details view file 'LICENSE'
 //SOME SHARED INFOS
 
 
-#define VERSION "0.43"
+#define VERSION "0.44"
 #define COPYRIGHT "Copyright (c) 2015 Marius Messerschmidt"
 #define DISCRIPTION "Simple Desktop Enviroment"
 
