54e5f96864b552a300ad287f51a1767dff02187d
Marius Messerschmidt <marius.messerschmidt@googlemail.com>
2015-09-08 13:02:18 +0200

    Version 0.82
    Added MimeType for side-view
    
    Signed-off-by: Marius Messerschmidt <marius.messerschmidt@googlemail.com>

--- shared/info.h
+++ shared/info.h
@@ -7,7 +7,7 @@ For more details view file 'LICENSE'
 //SOME SHARED INFOS
 
 //DESKTOP
-#define VERSION "0.81"
+#define VERSION "0.82"
 #define COPYRIGHT "Copyright (c) 2015 Marius Messerschmidt"
 #define DISCRIPTION "Simple Desktop Enviroment"
 
