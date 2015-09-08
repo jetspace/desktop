eea90aca7792c61060321dcec4b84a831c8d9a54
Marius Messerschmidt <marius.messerschmidt@googlemail.com>
2015-09-08 11:51:08 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -10,20 +10,39 @@ make
 make install
 make tester
 ./test
+if [ "$?" != "0" ]
+then
+exit 1
+fi
+
 cd ../side-config
 make
 make install
 make tester
 ./test
+if [ "$?" != "0" ]
+then
+exit 2
+fi
+
 cd ../side-log
 make
 make install
 make tester
 ./test
+if [ "$?" != "0" ]
+then
+exit 3
+fi
+
 cd ../side-plugin
 make
 make install
 make tester
 ./test
+if [ "$?" != "0" ]
+then
+exit 4
+fi
 
 echo "Testing done!"
