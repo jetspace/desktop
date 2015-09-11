865bcb757d9c4f47479ddb71e814c85658956b8a
Marius Messerschmidt <marius.messerschmidt@googlemail.com>
2015-09-11 22:12:06 +0200

    Version 0.84-4
    Updated Desktop Clock
    
    Signed-off-by: Marius Messerschmidt <marius.messerschmidt@googlemail.com>
    
    Signed-off-by: Marius Messerschmidt <marius.messerschmidt@googlemail.com>

--- plugins/desktop_clock.c
+++ plugins/desktop_clock.c
@@ -28,6 +28,13 @@ gboolean update_time(gpointer data);
 GtkWidget *side_desktop_clock, *box;
 GtkWidget *s_24, *s_sec, *s_utc;
 
+void center_side_desktop_clock(void)
+{
+  int width = gtk_widget_get_allocated_width(side_desktop_clock);
+  int height = gtk_widget_get_allocated_height(side_desktop_clock);
+  gtk_fixed_move(GTK_FIXED(box), side_desktop_clock, 1920/2-width/2,1080/2 - height / 2);
+}
+
 gboolean update_time(gpointer data)
 {
   if(!clock_enabled)
@@ -69,8 +76,8 @@ gboolean update_time(gpointer data)
     break;
   }
 
-
   gtk_label_set_text(GTK_LABEL(side_desktop_clock), time_str);
+  center_side_desktop_clock();
   return TRUE;
 }
 //will be called when clock is destroyed
@@ -89,9 +96,13 @@ gboolean redraw(GtkWidget *widget, GdkEvent  *event, gpointer user_data)
 void show_clock(void)
 {
   side_desktop_clock = gtk_label_new("");
+  gtk_widget_set_name(side_desktop_clock, "SIDEDESKTOPCLOCKPLUGINLABEL");
+
+
 
 
   gtk_fixed_put(GTK_FIXED(box), side_desktop_clock, 0,0);
+  center_side_desktop_clock();
   g_signal_connect(G_OBJECT(side_desktop_clock), "destroy", G_CALLBACK(redraw), NULL);
 }
 
@@ -107,6 +118,18 @@ G_MODULE_EXPORT void plugin_call(GtkWidget *root)
   clock_can_be_activated = TRUE;
   g_print("------------------------------\n-> SIDE Desktop clock loading...\n------------------------------\n"); //notify the user...
   box = root;
+
+
+  GtkCssProvider *p = gtk_css_provider_new();
+  GdkDisplay *display;
+  GdkScreen *screen;
+  display = gdk_display_get_default ();
+  screen = gdk_display_get_default_screen (display);
+  gtk_style_context_add_provider_for_screen (screen, GTK_STYLE_PROVIDER (p), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+  gtk_css_provider_load_from_data(p, "#SIDEDESKTOPCLOCKPLUGINLABEL \n{\nbackground-color: transparent;\ncolor: white;\nfont-size: 150px;\nfont-weight: 100;\n}", -1, NULL);
+
+
+
 }
 
 G_MODULE_EXPORT void enable_plugin(GtkWidget *root)
