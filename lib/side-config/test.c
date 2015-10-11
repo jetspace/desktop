4aefd2584ce1171070916530beba050d5c97b0bc
Marius Messerschmidt <marius.messerschmidt@googlemail.com>
2015-10-11 18:12:04 +0200

    Fixed testing tool for SiDEConfig
    Signed-off-by: Marius Messerschmidt <marius.messerschmidt@googlemail.com>

--- lib/side-config/test.c
+++ lib/side-config/test.c
@@ -4,8 +4,8 @@
 
 int main(void)
 {
-  side_dump_config_file("test.conf");
-  side_lookup_value("test.conf", "a");
-  side_set_value("test.conf", "ABC", "123", true);
+  side_dump_config_file("lib/side-config/test.conf");
+  side_lookup_value("lib/side-config/test.conf", "a");
+  side_set_value("lib/side-config/test.conf", "ABC", "123", true);
   return 0;
 }
