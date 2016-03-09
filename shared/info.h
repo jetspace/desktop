2322b86f68dd264ceed8684d006dd20416b15880
Marius Messerschmidt <marius.messerschmidt@googlemail.com>
2016-03-09 18:47:52 +0100

    Version 0.93-8
    First Sorting works + statusbar
    
    Signed-off-by: Marius Messerschmidt <marius.messerschmidt@googlemail.com>

--- shared/info.h
+++ shared/info.h
@@ -7,7 +7,7 @@ For more details view file 'LICENSE'
 //SOME SHARED INFOS
 
 //DESKTOP
-#define VERSION "0.93-7"
+#define VERSION "0.93-8"
 #define COPYRIGHT "Copyright (c) 2015 Marius Messerschmidt"
 #define DISCRIPTION "Simple Desktop Enviroment"
 
--- tools/taskmanager/taskmanager.c
+++ tools/taskmanager/taskmanager.c
@@ -11,10 +11,31 @@ typedef struct
   int size;
 }PIDContainer;
 
+typedef struct
+{
+  GtkWidget *window;
+  GtkListStore *store;
+  GtkWidget *statusbar;
+  GtkTreeModel *sort;
+  int sort_col;
+}SiDETaskmanagerProto;
+
+enum
+{
+  SIDE_TASKMANAGER_CONTEXT_PROCESSES = 0
+};
+
+enum
+{
+  COL_PID = 0,
+  COL_NAME,
+  COL_STATUS
+};
+
 gboolean update_rows(GtkTreeModel *model, GtkTreePath *path, GtkTreeIter *iter, gpointer data)
 {
   int pid;
-  gtk_tree_model_get(model, iter, 0, &pid, -1);
+  gtk_tree_model_get(model, iter, COL_PID, &pid, -1);
 
   PIDContainer *pc = (PIDContainer *) data;
 
@@ -29,23 +50,25 @@ gboolean update_rows(GtkTreeModel *model, GtkTreePath *path, GtkTreeIter *iter, 
         temp = _("Sleeping");
       else if(p->state == 'R')
         temp = _("Running");
-      if(p->state == 'Z')
+      else if(p->state == 'Z')
         temp = _("Zombie");
+      else
+        temp = _("Unknown");
 
-      gtk_list_store_set(GTK_LIST_STORE(model), iter, 0, pid,1, p->name, 2,temp, -1);
+      gtk_list_store_set(GTK_LIST_STORE(model), iter, COL_PID, pid,COL_NAME, p->name, COL_STATUS,temp, -1);
       free(p);
       return FALSE;
     }
   }
   gtk_list_store_remove(GTK_LIST_STORE(model), iter);
-
   return FALSE;
 }
 
 
 gboolean update_processes(gpointer l)
 {
-  GtkListStore *list = GTK_LIST_STORE(l);
+  SiDETaskmanagerProto *taskmanager = (SiDETaskmanagerProto *) l;
+  GtkListStore *list = GTK_LIST_STORE(taskmanager->store);
 
   PIDContainer *data = malloc(sizeof(*data));
   data->pids = jetspace_get_pids(&data->size);
@@ -65,14 +88,36 @@ gboolean update_processes(gpointer l)
         temp = _("Running");
       if(p->state == 'Z')
         temp = _("Zombie");
+      else
+        temp = _("Unknown");
+
 
       gtk_list_store_append(GTK_LIST_STORE(list), &iter);
-      gtk_list_store_set(GTK_LIST_STORE(list), &iter, 0, data->pids[x],1, p->name, 2,temp, -1);
+      gtk_list_store_set(GTK_LIST_STORE(list), &iter, COL_PID, data->pids[x],COL_NAME, p->name, COL_STATUS,temp, -1);
       free(p);
     }
   }
 
+  GtkStatusbar *sb = GTK_STATUSBAR(taskmanager->statusbar);
+  gchar *temp = g_strdup_printf("%d processes", data->size);
+  gtk_statusbar_remove_all(sb, SIDE_TASKMANAGER_CONTEXT_PROCESSES);
+  gtk_statusbar_push(sb, SIDE_TASKMANAGER_CONTEXT_PROCESSES , temp);
+  g_free(temp);
 
+  free(data->pids);
+  free(data);
+}
+
+void change_sort(GtkTreeViewColumn *col, gpointer data)
+{
+  SiDETaskmanagerProto *taskmanager = (SiDETaskmanagerProto *) data;
+  const gchar *title = gtk_tree_view_column_get_title(col);
+  if(strcmp(title, _("PID")) == 0)
+    taskmanager->sort_col = COL_PID;
+  else if(strcmp(title, _("Process Name")) == 0)
+    taskmanager->sort_col = COL_NAME;
+  else if(strcmp(title, _("Status")) == 0)
+    taskmanager->sort_col = COL_STATUS;
 }
 
 
@@ -80,14 +125,16 @@ int main(int argc, char **argv)
 {
   gtk_init(&argc, &argv);
 
-  GtkWidget *win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-  gtk_window_set_title(GTK_WINDOW(win),_("SiDE Taskmanager"));
+  SiDETaskmanagerProto *taskmanager = malloc(sizeof(*taskmanager));
+
+  taskmanager->window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_title(GTK_WINDOW(taskmanager->window),_("SiDE Taskmanager"));
 
   GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
-  gtk_container_add(GTK_CONTAINER(win), box);
+  gtk_container_add(GTK_CONTAINER(taskmanager->window), box);
 
   GtkTreeIter iter;
-  GtkListStore *list = gtk_list_store_new(3, G_TYPE_INT, G_TYPE_STRING, G_TYPE_STRING);
+  taskmanager->store = gtk_list_store_new(3, G_TYPE_INT, G_TYPE_STRING, G_TYPE_STRING);
 
 
   GtkWidget *tree = gtk_tree_view_new();
@@ -103,23 +150,38 @@ int main(int argc, char **argv)
 
   renderer = gtk_cell_renderer_text_new();
   column = gtk_tree_view_column_new_with_attributes(_("PID"), renderer, "text", 0, NULL);
+  g_signal_connect(column, "clicked", G_CALLBACK(change_sort), taskmanager);
+  gtk_tree_view_column_set_clickable(column, TRUE);
   gtk_tree_view_append_column(GTK_TREE_VIEW(tree), column);
 
   renderer = gtk_cell_renderer_text_new();
   column = gtk_tree_view_column_new_with_attributes(_("Process Name"), renderer, "text", 1, NULL);
+  g_signal_connect(column, "clicked", G_CALLBACK(change_sort), taskmanager);
+  gtk_tree_view_column_set_clickable(column, TRUE);
   gtk_tree_view_append_column(GTK_TREE_VIEW(tree), column);
 
   renderer = gtk_cell_renderer_text_new();
   column = gtk_tree_view_column_new_with_attributes(_("Status"), renderer, "text", 2, NULL);
+  g_signal_connect(column, "clicked", G_CALLBACK(change_sort), taskmanager);
+  gtk_tree_view_column_set_clickable(column, TRUE);
   gtk_tree_view_append_column(GTK_TREE_VIEW(tree), column);
 
-  gtk_tree_view_set_model(GTK_TREE_VIEW(tree), GTK_TREE_MODEL(list));
+
+  taskmanager->sort = gtk_tree_model_sort_new_with_model(GTK_TREE_MODEL(taskmanager->store));
+
+
+
+  gtk_tree_view_set_model(GTK_TREE_VIEW(tree), GTK_TREE_MODEL(taskmanager->sort));
   gtk_box_pack_start(GTK_BOX(box), scroll_win, TRUE, TRUE, 0);
 
-  g_timeout_add(1000, update_processes, list);
-  g_signal_connect_swapped(win, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-  gtk_widget_show_all(win);
+  taskmanager->statusbar = gtk_statusbar_new();
+  gtk_box_pack_end(GTK_BOX(box), taskmanager->statusbar, FALSE, FALSE, 0);
+
+  g_timeout_add(1000, update_processes, taskmanager);
+  g_signal_connect_swapped(taskmanager->window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+  gtk_widget_show_all(taskmanager->window);
 
   gtk_main();
 }
